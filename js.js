6d498617852f6533c237124b5c4364116b467d27
Milton Barrera <miltonjosue2001@gmail.com>
2020-09-20 15:45:01 -0600

    Se agregó el mediaQuery desde js, y la validación

--- js.js
+++ js.js
@@ -1,10 +1,24 @@
 const menu = document.querySelector('.menu');
 const burgerButton = document.querySelector('#burger-menu');
-
+const ipad = window.matchMedia('screen and (max-width:767px)');
 
 console.log(menu);
 
-burgerButton.addEventListener('click', hideShow);
+ipad.addListener(validation);
+
+if (ipad.matches) {
+    burgerButton.addEventListener('click', hideShow);
+}
+
+function validation(event) {
+    console.log(event.matches);
+    if (event.matches) {
+        burgerButton.addEventListener('click', hideShow);
+    }
+    else {
+        burgerButton.removeEventListener('click', hideShow);
+    }
+} 
 
 function hideShow() {
     if (menu.classList.contains('is-active')){
